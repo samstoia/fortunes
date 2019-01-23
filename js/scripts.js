a04c0fce48b925b62a63d6a5ad11004ee0145835
Aaron Taylor and Sam Stoia <aaron@>
2019-01-23 15:34:06 -0800

    return fortune using if then

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,37 @@ $(document).ready(function(){
     $("input:checkbox[name="+array[i]+"]:checked").each(function(){
       var ownership = parseInt($(this).val());
       total += ownership;
-    });
+      });
 
-  };
-alert(total)
+    };
+
+    if (total > 0 && total <= 5) {
+      $("#fortune1").show();
+      $("#fortune2").hide();
+      $("#fortune3").hide();
+      $("#fortune4").hide();
+    }
+    else if (total > 5 && total < 11) {
+      $("#fortune2").show();
+      $("#fortune1").hide();
+      $("#fortune3").hide();
+      $("#fortune4").hide();
+    }
+    else if (total > 10 && total < 16) {
+      $("#fortune3").show();
+      $("#fortune1").hide();
+      $("#fortune2").hide();
+      $("#fortune4").hide();
+    }
+    else if (total > 15 && total <= 20) {
+      $("#fortune4").show();
+      $("#fortune1").hide();
+      $("#fortune2").hide();
+      $("#fortune3").hide();
+    }
+    else if (total = 0) {
+      alert("Choose at least one box to receive a fortune!")
+    }
 });
 
 });
